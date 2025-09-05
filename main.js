d71348f459caca9435d6fcb10b0eb2d3f06ec739
MamyrUly <mamusenalikhan.com>
2025-09-05 19:09:43 +0500

    asdfda

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function calculate() {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
+  const num1 = Number(document.getElementById("num1").value);
+  const num2 = Number(document.getElementById("num2").value);
   const operator = document.getElementById("operator").value;
   const output = document.getElementById("output");
 
@@ -9,19 +9,17 @@ async function calculate() {
   try {
     const response = await fetch("/api/index", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ num1: 5, num2: 2, operator: "+" }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ num1, num2, operator }),
     });
 
     const data = await response.json();
 
     if (response.ok) {
-      console.log(data.result, typeof data.result);
+      console.log("✅ Результат:", data.result, typeof data.result);
       output.innerHTML = `<div class="result">ответ ${data.result}</div>`;
     } else {
-      output.innerHTML = `<div class="error">ошибка  ${data.error}</div>`;
+      output.innerHTML = `<div class="error">ошибка ${data.error}</div>`;
     }
   } catch (err) {
     output.innerHTML = `<div class="error">Ошибка соединения с сервером</div>`;
